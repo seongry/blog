3a42bcb0c0c65e4e50f0f9fdbaa704fab08a4d9f
룡이 <vev4747@gmail.com>
2019-03-14 00:31:24 +0900

    아이콘 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/koala-donut.png`,
       },
     },
     `gatsby-plugin-offline`,
