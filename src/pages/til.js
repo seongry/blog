1a02c11a8ac9b2742c446708a6295a7e28044edc
룡이 <vev4747@gmail.com>
2019-03-15 00:31:41 +0900

    Create til.js

---
+++ src/pages/til.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class TilIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return(
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="TIL"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`, `TIL`]}
+        />
+        <div>
+          TIL
+        </div>
+        <Bio />
+      </Layout>
+    )
+  }
+}
+
+export default TilIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
