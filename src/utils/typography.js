f0a41f9b18c072127724b39688a428a7d9ae07db
룡이 <vev4747@gmail.com>
2019-03-16 21:33:42 +0900

    구글 폰트 변경

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,23 +1,20 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
 
-delete Wordpress2016.googleFonts
+const typography = new Typography({
+  baseFontSize: '16px',
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    'Nanum Myeongjo'
+  ],
+  bodyFontFamily: ['Nanum Gothic Coding'],
+})
 
-const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
 }
 
+export const { scale, rhythm, options } = typography
 export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
