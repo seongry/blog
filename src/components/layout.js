623c5b9f3cb10b62eb26ea0cc84ab0dcd8774ae5
룡이 <vev4747@gmail.com>
2019-03-16 22:53:37 +0900

    헤더 수정

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,44 +11,63 @@ class Layout extends React.Component {
 
     if (location.pathname === rootPath) {
       header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
+        <header className={`section__header`}>
+          <div>
+            <h1
+              style={{
+                ...scale(1.5),
+                marginBottom: rhythm(1.5),
+                marginTop: 0,
+              }}
+            >
+              <Link
+                style={{
+                  boxShadow: `none`,
+                  textDecoration: `none`,
+                  color: `inherit`,
+                }}
+                to={`/`}
+              >
+                {title}✨
+              </Link>
+            </h1>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to={`/til`}>
+                  TIL
+                </Link>
+              </li>
+              <li>
+                <Link to={`/til`}>
+                  portfolio
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </header>
       )
     } else {
       header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
+        <header className={`section__header`}>
+          <h3
             style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
+              marginTop: 0,
             }}
-            to={`/`}
           >
-            {title}✨
-          </Link>
-        </h3>
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`,
+              }}
+              to={`/`}
+            >
+              {title}✨
+            </Link>
+          </h3>
+        </header>
       )
     }
     return (
@@ -60,12 +79,10 @@ class Layout extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        <header>{header}</header>
+        {header}
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          raina + gatsby
         </footer>
       </div>
     )
