3307affb3c228349d1061e37a4157c68960fd84c
룡이 <vev4747@gmail.com>
2019-03-23 00:40:21 +0900

    til 수정

--- src/pages/til.js
+++ src/pages/til.js
@@ -14,10 +14,17 @@ class TilIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="TIL"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`, `TIL`]}
+          keywords={[`blog`, `TIL`]}
         />
         <div>
-          준비중
+          <h3>🚀 공부한 것들을 기록해두며 성취감을 느끼기 위한 공간 🚀</h3>
+
+          <h4># 2019-03-23</h4>
+          <ul>
+            <li>gatsby 블로그 오픈하는 법 글 마무리</li>
+            <li>gatsby 블로그 애날리스틱 추가</li>
+            <li>gatsby 블로그 disqus 추가</li>
+          </ul>
         </div>
         <Bio />
       </Layout>
