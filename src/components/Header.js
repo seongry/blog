7060a37face83a067a41cc585b3bacc02b51d748
SeongRyeong Kim <vev4747@github.com>
2020-05-06 00:59:13 +0900

    github 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
-import React from "react"
-import { Link } from "gatsby"
-import styled from "styled-components"
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 
 const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   font-weight: 900;
   font-size: 20px;
   padding: 0.833rem 1.2rem;
@@ -11,10 +16,12 @@ const StyledHeader = styled.header`
   background: #ffffff;
   z-index: 99;
 
-  :hover {
-    color: #9332fd;
+  a {
+    :hover {
+      color: #9332fd;
+    }
   }
-`
+`;
 
 export const Header = () => {
   return (
@@ -23,12 +30,14 @@ export const Header = () => {
         style={{
           boxShadow: `none`,
           textDecoration: `none`,
-          color: `inherit`,
         }}
         to={`/`}
       >
         코알 데브 로그
       </Link>
+      <a href="https://github.com/raina94">
+        <FontAwesomeIcon icon={faGithubAlt} />
+      </a>
     </StyledHeader>
-  )
-}
+  );
+};
