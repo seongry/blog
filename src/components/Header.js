57baf2afc0d3ba617aa3dd83b0322e1813172268
SeongRyeong Kim <vev4747@gmail.com>
2021-05-05 18:35:45 +0900

    style: Change Header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,7 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  font-family: "IBMPlexSansKR-Bold";
-  font-size: 20px;
+  justify-content: center;
   padding: 0.833rem 1.2rem;
   position: sticky;
   top: 0;
@@ -24,21 +21,33 @@ const StyledHeader = styled.header`
   }
 `;
 
+const HeaderContents = styled.div`
+  width: 100%;
+  max-width: 800px;
+
+  display: flex;
+  justify-content: space-between;
+  font-family: "IBMPlexSansKR-Bold";
+  font-size: 20px;
+`;
+
 export const Header = () => {
   return (
     <StyledHeader>
-      <Link
-        style={{
-          boxShadow: `none`,
-          textDecoration: `none`,
-        }}
-        to={`/`}
-      >
-        코알 데브 로그
-      </Link>
-      <a href="https://github.com/seongry">
-        <FontAwesomeIcon icon={faGithubAlt} />
-      </a>
+      <HeaderContents>
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+          }}
+          to={`/`}
+        >
+          코알 데브 로그
+        </Link>
+        <a href="https://github.com/seongry">
+          <FontAwesomeIcon icon={faGithubAlt} />
+        </a>
+      </HeaderContents>
     </StyledHeader>
   );
 };
